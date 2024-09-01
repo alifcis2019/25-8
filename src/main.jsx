7bfeb68c07c3c64875af82e30a60312013a4a200
Ali Talaat <alitalaatali30@gmail.com>
2024-09-01 15:50:05 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import Home from './assets/views/Home'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home/>
   </StrictMode>,
 )
